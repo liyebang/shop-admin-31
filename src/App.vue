8830a14905326c2beeb3531d971eb62e414bd487
liyebang <1114084099@qq.comgit config --global user.email 1114084099@qq.com>
2019-06-06 15:36:27 +0800

    '第一次提交'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    index
+     <el-button type="primary" plain>主要按钮</el-button>
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  mounted(){
+    // //请求测试接口
+    // axios({
+    //   url:'https://api.github.com/users',
+    //   method:'GET',
+    //   //get请求参数
+    //   params:{
+    //     abc:213
+    //   }
+    // }).then(res => {
+    //   console.log(res);
+    // })
+
+
+    //优化的最终效果
+    this.$axios({
+      url:'https://api.github.com/users',
+      method:'GET',
+      //get请求参数
+      params:{
+        abc:213
+      }
+    }).then(res => {
+      console.log(res);
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//1.1导入组件
+import ElementUi from 'element-ui'
+import axios from 'axios'
+import VueRouter from 'vue-router'
+
+//1.2 element 样式y
+import 'element-ui/lib/theme-chalk/index.css';
+
+
+//导入页面组件
+import Login from './pages/Login.vue'
+
+//2注册组件
+Vue.use(ElementUi);
+
+//注册路由
+Vue.use(VueRouter)
+
+//配置路由
+const routes = [
+  { path:'/login', component: Login }
+]
+
+//路由实例
+const router = new VueRouter({ routes })
+
+Vue.config.productionTip = false;
+
+//给Vue的原型绑定axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
+  //挂载路由
+  router
 }).$mount('#app')
